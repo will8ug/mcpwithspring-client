8420d76e8c5fc4fa9258a1a8e9b7d1124f806d7c
Will <yzj_000@163.com>
2025-08-01 08:14:22 +0800

    Initial version

---
+++ src/main/java/io/will/mcpwithspring/client/McpwithspringClientApplication.java
@@ -0,0 +1,13 @@
+package io.will.mcpwithspring.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpwithspringClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpwithspringClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/will/mcpwithspring/client/McpwithspringClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.will.mcpwithspring.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpwithspringClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
